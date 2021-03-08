91a1b9044de1dd10d3d829730031c9ec0b3dda2f
Jcy <78772079+Jcy-xs@users.noreply.github.com>
2021-03-08 17:48:13 +0800

    111

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,72 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-03-08 17:01:30
+ * @LastEditTime: 2021-03-08 17:44:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \supermall01\src\components\content\tabControl\TabControl.vue
+-->
+<template>
+  <div class="tab-control">
+    <div
+      v-for="(item, index) in titles"
+      :key="index"
+      class="tab-control-item"
+      :class="{ active: index === currentIndex}"
+      @click="itemClick(index)"
+    >
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  height: 40px;
+  line-height: 40px;
+  background-color: #fff;
+}
+
+.tab-control-item {
+  flex: 1;
+}
+
+.tab-control-item span {
+  padding: 5px;
+}
+
+.active {
+  color: var(--color-high-text);
+}
+
+.active span {
+  border-bottom: 2px solid var(--color-tint);
+}
+</style>
---
+++ src/views/home/chukdComps/FeatureView.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-03-08 16:08:28
+ * @LastEditTime: 2021-03-08 16:17:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \supermall01\src\views\home\chukdComps\FeatureView.vue
+-->
+<template>
+  <div class="feature">
+    <a href="https://act.mogujie.com/zzlx67">
+    <img src="~assets/img/home/recommend_bg.jpg" alt=""></a>
+  </div>
+</template>
+
+<script>
+export default {
+name: "FeatureView"
+}
+</script>
+
+<style scoped>
+.feature img {
+  width: 100%;
+}
+</style>
